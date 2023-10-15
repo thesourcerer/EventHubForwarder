8f9fae095a1ac5d358f1b1aa9f9821a5b4a11285
Eli Ofek <elofek@microsoft.com>
2023-10-15 05:12:43 +0300

    Better utilization with batching.

--- src/EventHubProducer.cs
+++ src/EventHubProducer.cs
@@ -6,12 +6,21 @@ namespace EventHubForwarder
 {
     public static class EventHubProducer
     {
-        private static EventHubProducerClient? _producerClient;
+        private static EventHubBufferedProducerClient? _producerClient;
+        private static long _totalReceived;
+        private static long _totalSent;
+        private static long _totalFailed;
+        private static long _lastTotalSent;
+        private static DateTime _lastStatsTime = DateTime.UtcNow;
 
         public static void InitProducer(string? connectionString)
         {
-            _producerClient = new EventHubProducerClient(connectionString);
+            _producerClient = new EventHubBufferedProducerClient(connectionString);
+            _producerClient.SendEventBatchFailedAsync += ProducerClient_SendEventBatchFailedAsync;
+            _producerClient.SendEventBatchSucceededAsync += ProducerClient_SendEventBatchSucceededAsync;
         }
+
+
 
         public static async void WriteToEventHub(byte[] bytes)
         {
@@ -20,13 +29,33 @@ namespace EventHubForwarder
                 return;
 
             }
-            using (var eventBatch = await _producerClient.CreateBatchAsync())
+
+            Interlocked.Increment(ref _totalReceived);
+
+            if (_totalReceived % 10000 == 0)
             {
-                eventBatch.TryAdd(new EventData(bytes));
-                await _producerClient.SendAsync(eventBatch);
+                var now = DateTime.UtcNow;
+                var sendRate = (long)((_totalSent - _lastTotalSent) / (now - _lastStatsTime).TotalSeconds);
+                Console.WriteLine($"{DateTime.UtcNow} : Dispatched={_totalReceived} Sent={_totalSent} Failed={_totalFailed} Rate:{sendRate}/sec");
+                _lastTotalSent = _totalSent;
+                _lastStatsTime = now;
             }
+
+            await _producerClient.EnqueueEventAsync(new EventData(bytes));
+        }
+        private static Task ProducerClient_SendEventBatchFailedAsync(SendEventBatchFailedEventArgs arg)
+        {
+            
+            Interlocked.Add(ref _totalFailed, arg.EventBatch.Count);
+            Console.WriteLine(arg.Exception);
+            return Task.CompletedTask;
         }
 
+        private static Task ProducerClient_SendEventBatchSucceededAsync(SendEventBatchSucceededEventArgs arg)
+        {
+            Interlocked.Add(ref _totalSent, arg.EventBatch.Count);
+            return Task.CompletedTask;
+        }
     }
 
 
--- src/Test/locustfile.py
+++ src/Test/locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between
 import json
 
 class WebsiteUser(HttpUser):
-    wait_time = between(0, 0.00167)  # Roughly equivalent to 600 requests per second
+    #wait_time = between(0, 0.00167)  # Roughly equivalent to 600 requests per second
 
     def on_start(self):
         with open('generated.json', 'r') as f:
